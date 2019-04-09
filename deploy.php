4d1b5435a2e25124a8f544a2700a1827f101af14
Tito Miguel Costa <titomiguelcosta@gmail.com>
2019-04-09 21:06:19 +1200

    added configuration for bitckuet pipelines

--- deploy.php
+++ deploy.php
@@ -2,26 +2,26 @@
 
 namespace Deployer;
 
-require 'recipe/symfony.php';
+require 'recipe/symfony4.php';
 
 set('application', 'groomingchimps:api');
 set('repository', 'git@bitbucket.org:groomingchimps/api.git');
-set('git_tty', true); 
+set('git_tty', true);
 set('keep_releases', 3);
-set('shared_dirs', ['vendor']);
+set('shared_dirs', ['var/log', 'var/sessions', 'config/jwt', 'vendor']);
 set('writable_dirs', ['var']);
+set('composer_action', 'install');
+set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --no-suggest');
 
 host('api.groomingchimps.titomiguelcosta.com')
-    ->stage('production')
-    ->set('deploy_path', '/mnt/websites/groomingchimps/prod/api')
-    ->set('shared_files', ['.env.prod'])
-;
-    
-
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-
+    ->user('ubuntu')
+    ->stage('prod')
+    ->set('deploy_path', '/mnt/websites/groomingchimps/api')
+    ->set('shared_files', ['.env.prod.local'])
+    ->set('http_user', 'ubuntu')
+    ->set('writable_mode', 'acl')
+    ->set('branch', 'master')
+    ->set('env', ['APP_ENV' => 'prod']);
 
 after('deploy:failed', 'deploy:unlock');
 before('deploy:symlink', 'database:migrate');
