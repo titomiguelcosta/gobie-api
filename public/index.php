d40db267748daa35c3ecfa107537b0fbcded6c7f
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-06-20 14:52:17 +0200

    load env vars using putenv

--- public/index.php
+++ public/index.php
@@ -5,9 +5,10 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+// We need to enable putenv/getenv otherwise AWS will not work
+(new Dotenv())->usePutenv()->bootEnv(dirname(__DIR__) . '/.env');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
