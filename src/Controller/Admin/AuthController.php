ed984bfe82f631a8e4167662e2b468359756255a
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-06-24 19:08:53 +0200

    fixed class name

--- src/Controller/Admin/AuthController.php
+++ src/Controller/Admin/AuthController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class AdminController extends AbstractController
+class AuthController extends AbstractController
 {
     /**
      * @Route("/admin/login", name="admin_login")
