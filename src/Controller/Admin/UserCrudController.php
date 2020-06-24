65f9569a885a1062534d8ec817265991fb8c595c
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-06-24 19:33:24 +0200

    remove id from field list

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -19,7 +19,6 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('username'),
             EmailField::new('email'),
             TextField::new('plainPassword'),
