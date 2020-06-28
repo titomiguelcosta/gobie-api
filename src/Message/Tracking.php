02293ce42ebfa629b90a4e0dbf83b7a40f796c51
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-06-28 19:42:03 +0200

    make tracking async

--- src/Message/Tracking.php
+++ src/Message/Tracking.php
@@ -9,8 +9,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class Tracking
 {
-    private $request;
-    private $response;
+    private $routeName;
+    private $queryParameters;
+    private $requestHeaders;
+    private $requestMethod;
+    private $requestFormat;
+    private $requestBody;
+    private $requestPathInfo;
+    private $ipAddress;
+    private $locale;
+    private $device;
+    private $navigator;
+    private $responseBody;
+    private $responseStatusCode;
+    private $responseStatusText;
     private $startedAt;
     private $terminatedAt;
     private $userId;
@@ -22,21 +34,23 @@ final class Tracking
         DateTimeInterface $terminatedAt,
         ?User $user
     ) {
-        $this->request = $request;
-        $this->response = $response;
+        $this->routeName = $request->attributes->get('_route', 'unknown');
+        $this->queryParameters = $request->query->all();
+        $this->requestHeaders = $request->headers->all();
+        $this->requestMethod = $request->getRealMethod();
+        $this->requestFormat = $request->getRequestFormat();
+        $this->requestBody = $request->getContent();
+        $this->requestPathInfo = $request->getPathInfo();
+        $this->ipAddress = $request->getClientIp();
+        $this->locale = $request->getLocale();
+        $this->device = '';
+        $this->navigator = $request->headers->get('User-Agent');
+        $this->responseBody = $response->getContent();
+        $this->responseStatusCode = $response->getStatusCode();
+        $this->responseStatusText = Response::$statusTexts[$response->getStatusCode()] ?? 'unknown status';
         $this->startedAt = $startedAt;
         $this->terminatedAt = $terminatedAt;
         $this->userId = $user instanceof User ? $user->getId() : null;
-    }
-
-    public function getRequest(): Request
-    {
-        return $this->request;
-    }
-
-    public function getResponse(): Response
-    {
-        return $this->response;
     }
 
     public function getStartedAt(): DateTimeInterface
@@ -52,5 +66,75 @@ final class Tracking
     public function getUserId(): ?int
     {
         return $this->userId;
+    }
+
+    public function getRouteName(): ?string
+    {
+        return $this->routeName;
+    }
+
+    public function getQueryParameters(): array
+    {
+        return $this->queryParameters;
+    }
+
+    public function getRequestHeaders(): array
+    {
+        return $this->requestHeaders;
+    }
+
+    public function getRequestMethod(): string
+    {
+        return $this->requestMethod;
+    }
+
+    public function getRequestFormat(): string
+    {
+        return $this->requestFormat;
+    }
+
+    public function getRequestBody(): ?string
+    {
+        return $this->requestBody;
+    }
+
+    public function getRequestPathInfo(): string
+    {
+        return $this->requestPathInfo;
+    }
+
+    public function getIpAddress(): ?string
+    {
+        return $this->ipAddress;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function getDevice(): ?string
+    {
+        return $this->device;
+    }
+
+    public function getNavigator(): ?string
+    {
+        return $this->navigator;
+    }
+
+    public function getResponseBody(): string
+    {
+        return $this->responseBody;
+    }
+
+    public function getResponseStatusCode(): int
+    {
+        return $this->responseStatusCode;
+    }
+
+    public function getResponseStatusText(): string
+    {
+        return $this->responseStatusText;
     }
 }
--- src/MessageHandler/TrackingHandler.php
+++ src/MessageHandler/TrackingHandler.php
@@ -8,7 +8,6 @@ use App\Entity\Tracking;
 use App\Message\Tracking as Message;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class TrackingHandler implements MessageHandlerInterface
@@ -26,33 +25,30 @@ final class TrackingHandler implements MessageHandlerInterface
 
     public function __invoke(Message $message)
     {
-        $httpRequest = $message->getRequest();
-        $httpResponse = $message->getResponse();
-
         $tracking = new Tracking();
-        $tracking->setRouteName($httpRequest->attributes->get('_route', 'unknown'));
+        $tracking->setRouteName($message->getRouteName());
         $tracking->setStartedAt($message->getStartedAt());
         $tracking->setTerminatedAt($message->getTerminatedAt());
         if ($message->getUserId()) {
             $tracking->setUser($this->userRepository->find($message->getUserId()));
         }
-        $tracking->setNavigator($httpRequest->headers->get('User-Agent'));
-        $tracking->setIpAddress($httpRequest->getClientIp());
+        $tracking->setNavigator($message->getNavigator());
+        $tracking->setIpAddress($message->getIpAddress());
 
         $request = new Request();
-        $request->setQueryParameters($httpRequest->query->all());
-        $request->setHeaders($httpRequest->headers->all());
-        $request->setMethod($httpRequest->getRealMethod());
-        $request->setFormat($httpRequest->getRequestFormat());
-        $request->setBody($httpRequest->getContent());
-        $request->setPathInfo($httpRequest->getPathInfo());
-        $request->setLocale($httpRequest->getLocale());
+        $request->setQueryParameters($message->getQueryParameters());
+        $request->setHeaders($message->getRequestHeaders());
+        $request->setMethod($message->getRequestMethod());
+        $request->setFormat($message->getRequestFormat());
+        $request->setBody($message->getRequestBody());
+        $request->setPathInfo($message->getRequestPathInfo());
+        $request->setLocale($message->getLocale());
         $request->setTracking($tracking);
 
         $response = new Response();
-        $response->setStatusCode($httpResponse->getStatusCode());
-        $response->setStatusText(HttpFoundationResponse::$statusTexts[$httpResponse->getStatusCode()] ?? 'unknown status');
-        $response->setBody($response->getBody());
+        $response->setStatusCode($message->getResponseStatusCode());
+        $response->setStatusText($message->getResponseStatusText());
+        $response->setBody($message->getResponseBody());
         $response->setTracking($tracking);
 
         $this->entityManager->persist($tracking);
