d8b102e99e0575bfd9a14795b8974d0e4ab59ce2
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-07-01 23:21:01 +0200

    add annotation for param converter in job rerun controller

--- src/Controller/JobRerunController.php
+++ src/Controller/JobRerunController.php
@@ -9,9 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class JobRerunController extends AbstractController
 {
+    /**
+     * @ParamConverter("job", class=Job::class)
+     */
     public function __invoke(
         Request $request,
         Job $job,
