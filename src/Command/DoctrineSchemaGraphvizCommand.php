a927e71bc15868ffa5bd82e49f20b68e3914bfba
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-07-11 12:31:19 +0200

    added event entity and related classes

---
+++ src/Command/DoctrineSchemaGraphvizCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Doctrine\DBAL\Schema\Visitor\Graphviz;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class DoctrineSchemaGraphvizCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected static $defaultName = 'doctrine:schema:graphviz';
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct();
+        $this->em = $em;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Get dot from database schema')
+            ->setHelp(sprintf('Usage: bin/console %s > /tmp/db-schema.dot && dot -Tpdf /tmp/db-schema.dot > docs/db-schema.pdf && rm -f /tmp/db-schema.dot', $this->getName()));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->writeln($this->getDot());
+
+        return 0;
+    }
+
+    protected function getDot(): string
+    {
+        $schema = $this->em->getConnection()->getSchemaManager()->createSchema();
+        $visitor = new Graphviz();
+        $schema->visit($visitor);
+
+        return $visitor->getOutput();
+    }
+}
