eddfb4bb1a0f47379af6f2cb54acddd63a0ec966
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-07-23 22:17:05 +0200

    debug github request

--- src/Controller/GitHub/WebhookController.php
+++ src/Controller/GitHub/WebhookController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller\GitHub;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WebhookController extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     /**
      * @Route("/github/webhook", name="github_webhook", methods={"POST"})
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
+        $this->logger->warning('Debug GitHub request: ' . $request);
+
         return new Response('Got info.');
     }
 }
